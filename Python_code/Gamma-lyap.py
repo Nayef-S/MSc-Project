added new lit,fixed energy balance tex file and fixed gamma computation

Python_code/Gamma-lyap.py
@@ -11,6 +11,7 @@ from numpy.matlib import repmat
 from scipy import linalg
 
 
+
 Nx = 64
 Ny = 128
 delta = 1.0
@@ -26,44 +27,62 @@ dky = 2*math.pi/L/delta
 kky1= np.arange(0,Ny/2+1)
 kky2 = np.arange(-Ny/2+1,0)
 kky = np.concatenate((kky1, kky2), axis=None)*dky
-
+dy = L*delta/Ny
+yy = np.linspace(0,L*delta - dy,num=Ny)
 
 """ opening saved data """
 sigma = np.load('sigma.npy')
 U = np.load('U.npy')
 
 
-""" computing Gamma & C """
+""" Computing Gamma & C """
+
 Ck = np.zeros((128,128),dtype=np.complex)
-Chi = 2*sigma @ sigma.conj().T
 
-for k in tqdm(range(1,Nx+1)):
-
-    U_gamma = np.diag(U)
-    
-    U_ppgamma = np.diag(-kky**2 * U)
-    
-    A = np.fft.ifft2(1j * k * U_gamma)
-    
-    neglap = 1/(np.diag(kky**2) + np.identity(128)*(k**2))
-    
-    neglap[neglap == np.inf ] = 0
-    
-    B = np.fft.ifft2((1j*k*(np.identity(128)*beta + U_ppgamma)) * neglap)
-    
-    C = np.identity(128)*alp
-    
-    D = np.fft.ifft2(nu * (np.diag(kky**2) + np.identity(128)*(k**2))**p)
-    
-    Gamma_k = A + B + C + D 
-    
-    Ck += linalg.solve_continuous_lyapunov(Gamma_k, Chi)
-
-C2 = [Ck[i][i] for i in range(min(len(Ck[0]),len(Ck)))] # diagonal of C
-
-E_t0 = 1 - (nu/2)*((np.mean(np.real(C2))*L**2 *delta) - (L/alp)*(np.mean(np.real((kky*np.fft.ifft(U)))**2) * delta)) #stationary energy
+U_hat = np.fft.fft(U)
 
 
+def Gamma_kyap(sigma,U_hat,kx):
+    
+    sigma_real = np.real(np.fft.ifft(sigma[:,kx]))
+    
+    Chi = 2*sigma_real.reshape(Ny,1) @ sigma_real.reshape(Ny,1).conj().T
+     
+    A = np.diag(1j * kx * U)
+    
+    invlap = 1/((kky**2) + (kx**2))
+    
+    invlap[invlap == np.inf ] = 0
+    
+    B = 1j*kx* np.fft.ifft2(np.diag(-kky**2 * U_hat - beta))
+    
+    C = np.fft.ifft2(np.diag(invlap))
+    
+    D = np.identity(Ny)*alp
+    
+    E = np.fft.ifft2(nu * (np.diag(kky**2) + np.identity(Ny)*(kx**2))**p)
+    
+    Gamma_k = A + B*C + D + E 
+    
+    Ck = linalg.solve_continuous_lyapunov(Gamma_k, Chi)
+    
+    return Ck
+
+C_xpyp = []
+
+"""Computing stationary energy"""
+
+for kx in tqdm(range(0,Nx)):
+    
+    Ck = Gamma_kyap(sigma,U_hat,kx)
+
+    diagC = [Ck[i][i] for i in range(min(len(Ck[0]),len(Ck)))] # diagonal of C
+    
+    C_xpyp.append(diagC)
+
+E_st = 1 - (nu*0.5)*((np.mean(np.real(C_xpyp))*(L**2) *delta) + (L/alp)*(np.mean(np.real(np.fft.ifft(1j*kky*U_hat))**2) * delta)) #stationary energy
+
+""" computing meta stable states - forward euler"""
 
 
 

Python_code/Q-L_system.py
@@ -10,7 +10,7 @@ import matplotlib.animation as animation
 from scipy import linalg
 # import pyfftw
 #numerical parameters
-steps = int(5e6)
+steps = 5000
 Nx = 64
 Ny = 128
 dt = 1e-3
@@ -187,6 +187,7 @@ np.save('U.npy', np.real(np.fft.ifft(U)))
 np.save('Upp.npy', np.real(np.fft.ifft(-kky**2 * U)))
 np.save('w.npy', w) 
 np.save('sigma.npy', sigma) 
+np.save('E.npy', E) 
 
 # ani = animation.ArtistAnimation(fig, ims, interval=10, blit=True)
 