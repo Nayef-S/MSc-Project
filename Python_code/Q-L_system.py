f518b580033982bf8fd7d6ab2ae90c2345d0db7e
Nayef <n.shkeir@warwick.ac.uk>
2020-07-02 14:19:19 +0100

    Revert "added new lit,fixed energy balance tex file and fixed gamma computation"
    This reverts commit da1399c11e15664fc63e78fabd0aa634754ee049.

--- Python_code/Q-L_system.py
+++ Python_code/Q-L_system.py
@@ -10,7 +10,7 @@ import matplotlib.animation as animation
 from scipy import linalg
 # import pyfftw
 #numerical parameters
-steps = 5000
+steps = int(5e6)
 Nx = 64
 Ny = 128
 dt = 1e-3
@@ -187,7 +187,6 @@ np.save('U.npy', np.real(np.fft.ifft(U)))
 np.save('Upp.npy', np.real(np.fft.ifft(-kky**2 * U)))
 np.save('w.npy', w) 
 np.save('sigma.npy', sigma) 
-np.save('E.npy', E) 
 
 # ani = animation.ArtistAnimation(fig, ims, interval=10, blit=True)
 
